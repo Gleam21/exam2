2954195d238213e348e8dceaf93ab81769dfd6f3
Jaemin Jung <windyvill@gmail.com>
2019-11-20 17:49:09 +0900

    지구의 밤 낮 구현

---
+++ Assets/Scripts/Earth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour
+{
+    [SerializeField] float speed;
+  
+
+    // Update is called once per frame
+    void Update()
+    { 
+        transform.Rotate(Vector3.up, Mathf.PI);
+
+
+
+        float val = Mathf.PingPong(Time.time*speed, 1);
+
+        GetComponent<Renderer>().material.SetFloat("_TexValue", val);
+
+    }
+}
---
+++ Assets/Scripts/Moon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    
+    [SerializeField] GameObject Earth;
+    Vector3 tr;
+    [SerializeField] float speed;
+    float x, y;
+    void Start()
+    {
+
+        tr = Earth.GetComponent<Transform>().position;
+
+         
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(tr, Vector3.down, speed * Time.deltaTime);
+        transform.Rotate(Vector3.up, Mathf.PI);
+
+    }
+}
